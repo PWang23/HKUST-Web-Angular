b88c520c9dfc4a783d966cf64f9e914bc0be6d14
Yuanzhe Wang <yuanzhe.wang23@gmail.com>
2022-12-01 14:58:54 -0500

    footer and header

--- Angular/conFusion/src/app/menu/menu.component.ts
+++ Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -15,13 +15,14 @@ export class MenuComponent implements OnInit {
 
   selectedDish = DISHES[0];   */ 
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
    
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish) {
